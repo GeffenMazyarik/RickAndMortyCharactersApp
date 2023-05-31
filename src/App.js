f68abdade7909e66ec7a971a8885f4ecd2600c09
Geffen.Mazyarik <geffen.mazyarik@gmail.com>
2023-05-31 20:48:23 +0300

    Init repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button> Click </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
